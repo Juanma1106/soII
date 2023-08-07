f79d2ff4ef4b4d21a56aa95a721f685dcd061169
Sebastian Andres Meli <sebastianandresmeli@gmail.com>
2023-08-07 17:33:58 -0300

    plancha 3 terminada

--- nachos-unr22a/code/threads/thread.cc
+++ nachos-unr22a/code/threads/thread.cc
@@ -53,6 +53,7 @@ Thread::Thread(const char *threadName, bool isJoinable, Thread *father, int theP
 #ifdef USER_PROGRAM
     space = nullptr;
     spaceId = threads->Add(this);
+    openedFiles = new Table<OpenFile*>;
 #endif
 }
 
